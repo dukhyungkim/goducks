69a0e97bedbdab13c2dc7f76d28303481c633239
DukHyung Kim <idealismboy@gmail.com>
2023-06-11 13:53:20 +0900

    한글 조사 붙여주는 함수 추가

--- escape/util/hangul/hangul.go
+++ escape/util/hangul/hangul.go
@@ -4,11 +4,31 @@ import (
 	"unicode/utf8"
 )
 
-func Extract(s string) rune {
+type Josa string
+
+const (
+	EunNun Josa = "은/는"
+	LeeGa  Josa = "이/가"
+	WaGwa  Josa = "와/과"
+	EulLul Josa = "을/를"
+)
+
+var jaso = map[Josa]map[bool]string{
+	EunNun: {true: "은", false: "는"},
+	LeeGa:  {true: "이", false: "가"},
+	WaGwa:  {true: "과", false: "와"},
+	EulLul: {true: "을", false: "를"},
+}
+
+func extractLast(s string) rune {
 	last, _ := utf8.DecodeLastRune([]byte(s))
 	return last
 }
 
-func HasJongSung(r rune) bool {
+func hasJongSung(r rune) bool {
 	return (r-0xac00)%28 != 0
+}
+
+func WithJosa(s string, j Josa) string {
+	return s + jaso[j][hasJongSung(extractLast(s))]
 }
--- escape/util/hangul/hangul_test.go
+++ escape/util/hangul/hangul_test.go
@@ -2,7 +2,7 @@ package hangul
 
 import "testing"
 
-func TestExtract(t *testing.T) {
+func Test_extractLast(t *testing.T) {
 	type args struct {
 		s string
 	}
@@ -24,14 +24,14 @@ func TestExtract(t *testing.T) {
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if got := Extract(tt.args.s); got != tt.want {
-				t.Errorf("Extract() = %v, want %v", got, tt.want)
+			if got := extractLast(tt.args.s); got != tt.want {
+				t.Errorf("extractLast() = %v, want %v", got, tt.want)
 			}
 		})
 	}
 }
 
-func TestHasJongSung(t *testing.T) {
+func Test_hasJongSung(t *testing.T) {
 	type args struct {
 		r rune
 	}
@@ -53,8 +53,92 @@ func TestHasJongSung(t *testing.T) {
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if got := HasJongSung(tt.args.r); got != tt.want {
-				t.Errorf("HasJongSung() = %v, want %v", got, tt.want)
+			if got := hasJongSung(tt.args.r); got != tt.want {
+				t.Errorf("hasJongSung() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestWithJosa(t *testing.T) {
+	type args struct {
+		s string
+		j Josa
+	}
+	tests := []struct {
+		name string
+		args args
+		want string
+	}{
+		{
+			name: "다람쥐 은/는",
+			args: args{
+				s: "다람쥐",
+				j: EunNun,
+			},
+			want: "다람쥐는",
+		},
+		{
+			name: "다람쥐 이/가",
+			args: args{
+				s: "다람쥐",
+				j: LeeGa,
+			},
+			want: "다람쥐가",
+		},
+		{
+			name: "다람쥐 을/를",
+			args: args{
+				s: "다람쥐",
+				j: EulLul,
+			},
+			want: "다람쥐를",
+		},
+		{
+			name: "다람쥐 와/과",
+			args: args{
+				s: "다람쥐",
+				j: WaGwa,
+			},
+			want: "다람쥐와",
+		},
+		{
+			name: "사슴 은/는",
+			args: args{
+				s: "사슴",
+				j: EunNun,
+			},
+			want: "사슴은",
+		},
+		{
+			name: "사슴 이/가",
+			args: args{
+				s: "사슴",
+				j: LeeGa,
+			},
+			want: "사슴이",
+		},
+		{
+			name: "사슴 을/를",
+			args: args{
+				s: "사슴",
+				j: EulLul,
+			},
+			want: "사슴을",
+		},
+		{
+			name: "사슴 와/과",
+			args: args{
+				s: "사슴",
+				j: WaGwa,
+			},
+			want: "사슴과",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := WithJosa(tt.args.s, tt.args.j); got != tt.want {
+				t.Errorf("WithJosa() = %v, want %v", got, tt.want)
 			}
 		})
 	}
